49f0c62dbf4e732f3fd6ba699c168f4ffb5ac3d3
Judy McGoldrick <jjeanius@verizon.net>
2018-04-30 01:25:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,40 @@
-# Write your sql queries in this file in the appropriate method like the example below:
-#
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
-
-# Make sure each ruby method returns a string containing a valid SQL statement.
-
-def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+def select_name_users
+  "SELECT SUM(name) FROM users;"
 end
 
+def select_count_from_projects
+  "SELECT COUNT (*) FROM projects;"
+end
+
+def select_count_from_pledges
+  "SELECT COUNT(*) FROM pledges;"
+end
+
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
+  "SELECT title, SUM(pledges.amount) FROM projects
+  JOIN pledges ON pledges.project_id = project.id
+  ORDER BY name;"
+  end
+# SELECT aisle, SUM(quantity) FROM groceries GROUP BY aisle ORDER BY SUM(quantity);
+  # CREATE TABLE projects (id, title, category, funding_goal, start_date, end_date);
+  # CREATE TABLE users (id, name, age);
+  # CREATE TABLE pledges (id, amount, user_id, project_id);
+
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  "SELECT user name, age, pledges amount FROM users
+  JOIN users ON users.id = user_id
+  GROUCP BY project_id
+  ORDER BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
 end
