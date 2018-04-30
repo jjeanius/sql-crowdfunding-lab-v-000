08e38598ebcf9e491206f033ee1cdc9ac5071e88
Judy McGoldrick <jjeanius@verizon.net>
2018-04-30 01:46:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,9 +22,9 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   # CREATE TABLE pledges (id, amount, user_id, project_id);
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT  name, age, SUM(pledges.amount) FROM users
-  JOIN Pledges ON  pledges.id = users.id
-  GROUCP BY   ORDER BY user_id
+  "SELECT name, age, SUM(pledges.amount) FROM users
+  JOIN pledges ON  pledges.user_id = users.id
+  GROUP BY user_id
   ORDER BY name"
 end
 
